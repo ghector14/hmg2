b4cf2910a6ae1ed599ff02f96b6cd14d6fd592ac
ghector14 <gomezhector62@gmail.com>
2022-04-14 16:46:31 -0700

    add new code

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+import ReactTooltip from 'react-tooltip';
 
+import { AppWrap, appWrap } from '../../wrapper'
+import { urlFor, client } from '../../client'
 import './Skills.scss';
 
 const Skills = () => {
   return (
-    <div>Skills</div>
+    <>
+    <h2 className="head-text">Skills & Experience</h2>
+
+      <div className="app__skills-container">
+        <motion className="app__skills-list">
+
+        </motion>
+      </div>
+    </>
   )
 }
 
