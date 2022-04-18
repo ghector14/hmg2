98429ebac6f3ba8af33a91c7fb97f826cc68e409
ghector14 <gomezhector62@gmail.com>
2022-04-18 10:03:35 -0700

    add new files

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -1,53 +1,51 @@
 import React, { useState, useEffect } from 'react';
-import {AiFillEye, AiFillGithub } from 'react-icons/ai';
+import { AiFillEye, AiFillGithub } from 'react-icons/ai';
 import { motion } from 'framer-motion';
 
-import { AppWrap, MotionWrap } from '../../wrapper'
-import { urlFor, client } from '../../client'
+import { AppWrap, MotionWrap } from '../../wrapper';
+import { urlFor, client } from '../../client';
 import './Work.scss';
 
 const Work = () => {
-  const [activeFilter, setActiveFilter] = useState('All');
-  const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1});
   const [works, setWorks] = useState([]);
   const [filterWork, setFilterWork] = useState([]);
+  const [activeFilter, setActiveFilter] = useState('All');
+  const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
 
   useEffect(() => {
-    const query = '*[_type == "works"]'
+    const query = '*[_type == "works"]';
 
-    client.fetch(query)
-      .then((data) => {
-        setWorks(data);
-        setFilterWork(data);
-      })
-  }, [])
-  
+    client.fetch(query).then((data) => {
+      setWorks(data);
+      setFilterWork(data);
+    });
+  }, []);
 
   const handleWorkFilter = (item) => {
     setActiveFilter(item);
-    setAnimateCard([{y:100, opacity: 0}]);
+    setAnimateCard([{ y: 100, opacity: 0 }]);
 
     setTimeout(() => {
-      setAnimateCard([ { y: 0, opacity:1 }]);
+      setAnimateCard([{ y: 0, opacity: 1 }]);
 
-      if(item ==='All') {
+      if (item === 'All') {
         setFilterWork(works);
       } else {
-        setFilterWork(works.filter((work) => work.tags.imcludes(item)))
+        setFilterWork(works.filter((work) => work.tags.includes(item)));
       }
     }, 500);
-  }
+  };
 
   return (
     <>
       <h2 className="head-text">My Creative <span>Portfolio</span> Section</h2>
 
-      <div className="app-work-filter">
+      <div className="app__work-filter">
         {['UI/UX', 'Web App', 'Mobile App', 'React JS', 'All'].map((item, index) => (
-          <div 
+          <div
             key={index}
             onClick={() => handleWorkFilter(item)}
-            className={`app__work-filter app__flex p-text ${activeFilter === item ? 'item-active' : ''}`}
+            className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ''}`}
           >
             {item}
           </div>
@@ -57,23 +55,26 @@ const Work = () => {
       <motion.div
         animate={animateCard}
         transition={{ duration: 0.5, delayChildren: 0.5 }}
-          className="app__work-portfolio"
+        className="app__work-portfolio"
       >
         {filterWork.map((work, index) => (
           <div className="app__work-item app__flex" key={index}>
-            <div className="app__work-img app__flex">
+            <div
+              className="app__work-img app__flex"
+            >
               <img src={urlFor(work.imgUrl)} alt={work.name} />
 
               <motion.div
-                whileHover={{opacity: [0, 1]}}
-                transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.05 }}
+                whileHover={{ opacity: [0, 1] }}
+                transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
                 className="app__work-hover app__flex"
               >
                 <a href={work.projectLink} target="_blank" rel="noreferrer">
+
                   <motion.div
-                    whileInView={{scale: [0, 1]}}
-                    whileHover={{ scale: [1, 0.9]}}
-                    transition={{duration: 0.25 }}
+                    whileInView={{ scale: [0, 1] }}
+                    whileHover={{ scale: [1, 0.90] }}
+                    transition={{ duration: 0.25 }}
                     className="app__flex"
                   >
                     <AiFillEye />
@@ -81,9 +82,9 @@ const Work = () => {
                 </a>
                 <a href={work.codeLink} target="_blank" rel="noreferrer">
                   <motion.div
-                    whileInView={{scale: [0, 1]}}
-                    whileHover={{ scale: [1, 0.9]}}
-                    transition={{duration: 0.25 }}
+                    whileInView={{ scale: [0, 1] }}
+                    whileHover={{ scale: [1, 0.90] }}
+                    transition={{ duration: 0.25 }}
                     className="app__flex"
                   >
                     <AiFillGithub />
@@ -92,7 +93,7 @@ const Work = () => {
               </motion.div>
             </div>
 
-            <div className="app__work-content app_flex">
+            <div className="app__work-content app__flex">
               <h4 className="bold-text">{work.title}</h4>
               <p className="p-text" style={{ marginTop: 10 }}>{work.description}</p>
 
@@ -100,16 +101,15 @@ const Work = () => {
                 <p className="p-text">{work.tags[0]}</p>
               </div>
             </div>
-
           </div>
         ))}
       </motion.div>
     </>
-  )
-}
+  );
+};
 
 export default AppWrap(
   MotionWrap(Work, 'app__works'),
   'work',
-  "app__primarybg",
+  'app__primarybg',
 );
